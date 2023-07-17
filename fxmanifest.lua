5de06716e4893fd44025242729ac57d38b58f656
itzfil0zz <101278240+itzfil0zz@users.noreply.github.com>
2023-07-17 17:03:42 +0200

    Files Update

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'itz.fil0zz'
+description 'A new FiveM MLO situated in Vinewood!'
+version '1.0.0'
+
+
+this_is_a_map 'yes'
+
+
+
+data_file('DLC_ITYP_REQUEST')('stream/itzmapz-winewoodvilla-01.ytyp')
